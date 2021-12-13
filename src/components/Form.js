52a826855fb6ec722376a57f1744092ba3097cbc
mohammad khosrotabar <mh.khosrotabar75@gmail.com>
2021-12-14 00:01:10 +0330

    update

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,29 +1,29 @@
-import React, {useState} from 'react'
-const axios = require('axios')
+import React, { useState } from "react";
+const axios = require("axios");
 
 function Form(props) {
-    const [username, setUsername] = useState('')
-    
-    function handleSubmit(event) {
-        event.preventDefault()
-        axios.get(`https://api.github.com/${username}`).then(resp => {
-            props.onSubmit(resp.data)
-            setUsername('')
-        })
-    }
+  const [username, setUsername] = useState("");
 
-    return (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={username}
-            onChange={event => setUsername(event.target.value)}
-            placeholder="GitHub username"
-            required
-          />
-          <button type="submit">Show informations</button>
-        </form>
-    )
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios.get(`https://api.github.com/users/${username}`).then((resp) => {
+      props.onSubmit(resp.data);
+      setUsername("");
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+        placeholder="GitHub username"
+        required
+      />
+      <button type="submit">Show informations</button>
+    </form>
+  );
 }
 
-export default Form
+export default Form;
